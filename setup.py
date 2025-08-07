c90d99632e4fde5e56b6e769cc92d43a62970fc0
John tambwe kapita <johntambwekapita@Johns-Air.lan>
2025-08-07 19:48:41 +0200

    Fixed deployment dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='uwc-connect',
+    version='0.1.0',
+    install_requires=[
+        'streamlit==1.29.0',
+        'pandas==1.5.3',
+        'Pillow==10.4.0'
+    ],
+)
